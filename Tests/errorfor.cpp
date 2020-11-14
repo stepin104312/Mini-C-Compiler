945868b3467cfc5512a17b11293554bf467a1c07
stepin104312 <69116163+stepin104312@users.noreply.github.com>
2020-11-14 12:29:12 +0530

    Add files via upload

---
+++ Tests/errorfor.cpp
@@ -0,0 +1,12 @@
+
+int main () {
+	int av;
+	float a=1;
+	int sum=-a;
+	int y=10;
+	for(y=10;y|=0;y+1)	
+	{ cout<<0<<endl;
+	}
+	int d;
+	}
+ 
---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "y",
+  1: "y",
+  2: "y"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Variables"
+};
+
